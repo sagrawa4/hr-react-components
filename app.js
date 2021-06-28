27a883ee39a78842061c78151f760c92ea52b6dd
Shruti Agrawal <shruti98932@gmail.com>
2021-06-28 15:08:45 -0400

    Implemented a GroceryList with two Sub componenets

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-// TODO
+
+var Butter = () => {
+  return (
+  <div>
+    <li> SaltedButter </li>
+    <li> UnSaltedButter </li>
+  </div>
+  );
+}
+
+var Bread = () => {
+  return (
+  <div>
+    <li> SourBread </li>
+    <li> PepperBread </li>
+  </div>
+  );
+}
+
+var GroceryList = () => {
+  return (
+  <div>
+    <h2> My GroceryList</h2>
+    <Bread />
+    <Butter />
+  </div>
+  );
+}
+
+var App = () => {
+
+return (
+<div className = 'App'>
+<GroceryList />
+</div>
+);
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
